8b556ae5b07fd28c8d29121dd93eb6641ca5a6a8
TwentyFive <TwentyFiveSoftware@gmail.com>
2022-01-21 23:59:05 +0100

    states

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './styles/App.module.scss';
+import { transpile } from './logic/transpile';
 
 const App = () => {
+    const [influxQL, setInfluxQL] = useState<string>('');
+    const [flux, setFlux] = useState<string>('');
+
+    useEffect(() => setFlux(transpile(influxQL)), [influxQL]);
+
     return (
         <div className={styles.app}>
             <main className={styles.main}>
                 <h1 className={styles.headline}>InfluxQL to Flux transpiler</h1>
 
                 <div className={styles.inputContainer}>
-                    <textarea className={styles.input} rows={1} placeholder={'Type your InfluxQL query here'} />
+                    <textarea className={styles.input} rows={1} placeholder={'Type your InfluxQL query here'}
+                              value={influxQL} onChange={e => setInfluxQL(e.target.value)} />
                 </div>
 
-                <div className={styles.outputContainer}>
-                    <p className={styles.output}></p>
-                </div>
+                {flux.length > 0 && (
+                    <div className={styles.outputContainer}>
+                        <p className={styles.output}>{flux}</p>
+                    </div>
+                )}
             </main>
         </div>
     );
