c062ff0ea155cf2b89d007d3ba535ac591a1c68c
TwentyFive <TwentyFiveSoftware@gmail.com>
2022-01-21 23:54:29 +0100

    basic page layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,17 @@ import styles from './styles/App.module.scss';
 const App = () => {
     return (
         <div className={styles.app}>
-            App
+            <main className={styles.main}>
+                <h1 className={styles.headline}>InfluxQL to Flux transpiler</h1>
+
+                <div className={styles.inputContainer}>
+                    <textarea className={styles.input} rows={1} placeholder={'Type your InfluxQL query here'} />
+                </div>
+
+                <div className={styles.outputContainer}>
+                    <p className={styles.output}></p>
+                </div>
+            </main>
         </div>
     );
 };
