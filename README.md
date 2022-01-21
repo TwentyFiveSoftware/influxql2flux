46fb908a1f1fff0bc32be95f4d723587b8721b1b
TwentyFive <TwentyFiveSoftware@gmail.com>
2022-01-21 22:55:03 +0100

    setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# InfluxQL to Flux transpiler
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles/App.module.scss';
+
+const App = () => {
+    return (
+        <div className={styles.app}>
+            App
+        </div>
+    );
+};
+
+export default App;
