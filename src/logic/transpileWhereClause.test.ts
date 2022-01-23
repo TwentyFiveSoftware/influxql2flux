68fa91db6980e326fa8841d20ad1cedadebd2522
TwentyFive <TwentyFiveSoftware@gmail.com>
2022-01-23 16:40:37 +0100

    unit tests for where clause transpiling

---
+++ src/logic/transpileWhereClause.test.ts
@@ -0,0 +1,242 @@
+import { Condition, Filter, transpileWhereClause } from './transpileWhereClause';
+
+test('empty', () => {
+    const whereClause = transpileWhereClause(``);
+    expect(whereClause.filters).toBeUndefined();
+});
+
+test('equals', () => {
+    const whereClause = transpileWhereClause(`host = "frontend"`);
+    const expected: Filter = { fields: ['host'], operator: '==', value: '"frontend"' };
+    expect(whereClause.filters).toEqual(expected);
+});
+
+test('less', () => {
+    const whereClause = transpileWhereClause(`time   < '2022-01-01T00:00:00Z'`);
+    const expected: Filter = { fields: ['time'], operator: '<', value: '2022-01-01T00:00:00Z' };
+    expect(whereClause.filters).toEqual(expected);
+});
+
+test('greater equals', () => {
+    const whereClause = transpileWhereClause(`_value >= -25.25`);
+    const expected: Filter = { fields: ['_value'], operator: '>=', value: '-25.25' };
+    expect(whereClause.filters).toEqual(expected);
+});
+
+test('not equals (<>)', () => {
+    const whereClause = transpileWhereClause(`"impact location" <> "Europe"`);
+    const expected: Filter = { fields: ['"impact location"'], operator: '!=', value: '"Europe"' };
+    expect(whereClause.filters).toEqual(expected);
+});
+
+test('not equals (!=)', () => {
+    const whereClause = transpileWhereClause(`(("impact location" != "Europe"))`);
+    const expected: Filter = { fields: ['"impact location"'], operator: '!=', value: '"Europe"' };
+    expect(whereClause.filters).toEqual(expected);
+});
+
+test('regex (=~)', () => {
+    const whereClause = transpileWhereClause(`'host' =~ /^[a-z]+-eu$/`);
+    const expected: Filter = { fields: ['host'], operator: '=~', value: '/^[a-z]+-eu$/' };
+    expect(whereClause.filters).toEqual(expected);
+});
+
+test('regex (!~)', () => {
+    const whereClause = transpileWhereClause(`'host' !~   /^[0-9]+$/`);
+    const expected: Filter = { fields: ['host'], operator: '!~', value: '/^[0-9]+$/' };
+    expect(whereClause.filters).toEqual(expected);
+});
+
+test('multiple conditions (and) 1', () => {
+    const whereClause = transpileWhereClause(`'user agent' = 'Chrome' and region <> "Northern America"`);
+    const expected: Condition = {
+        type: 'and',
+        variables: [
+            { fields: ['"user agent"'], operator: '==', value: '"Chrome"' },
+            { fields: ['region'], operator: '!=', value: '"Northern America"' },
+        ],
+    };
+    expect(whereClause.filters).toEqual(expected);
+});
+
+test('multiple conditions (and) 2', () => {
+    const whereClause = transpileWhereClause(`a = 1 and b = 2 and c = 3`);
+    const expected: Condition = {
+        type: 'and',
+        variables: [
+            { fields: ['a'], operator: '==', value: '1' },
+            { fields: ['b'], operator: '==', value: '2' },
+            { fields: ['c'], operator: '==', value: '3' },
+        ],
+    };
+    expect(whereClause.filters).toEqual(expected);
+});
+
+test('multiple conditions (and) 3', () => {
+    const whereClause = transpileWhereClause(`(_value >= -3.5 and _value <= 9999) and time > now() - 7d`);
+    const expected: Condition = {
+        type: 'and',
+        variables: [
+            {
+                type: 'and',
+                variables: [
+                    { fields: ['_value'], operator: '>=', value: '-3.5' },
+                    { fields: ['_value'], operator: '<=', value: '9999' },
+                ],
+            },
+            { fields: ['time'], operator: '>', value: 'now() - 7d' },
+        ],
+    };
+    expect(whereClause.filters).toEqual(expected);
+});
+
+test('multiple conditions (or) 1', () => {
+    const whereClause = transpileWhereClause(`a = 1 or b = 2 or c = 3`);
+    const expected: Condition = {
+        type: 'or',
+        variables: [
+            { fields: ['a'], operator: '==', value: '1' },
+            { fields: ['b'], operator: '==', value: '2' },
+            { fields: ['c'], operator: '==', value: '3' },
+        ],
+    };
+    expect(whereClause.filters).toEqual(expected);
+});
+
+test('multiple conditions (or) 2', () => {
+    const whereClause = transpileWhereClause(`a = 1 or (b = 2 or (c = 3))`);
+    const expected: Condition = {
+        type: 'or',
+        variables: [
+            { fields: ['a'], operator: '==', value: '1' },
+            {
+                type: 'or',
+                variables: [
+                    { fields: ['b'], operator: '==', value: '2' },
+                    { fields: ['c'], operator: '==', value: '3' },
+                ],
+            },
+        ],
+    };
+    expect(whereClause.filters).toEqual(expected);
+});
+
+test('and/or precedence', () => {
+    const whereClause = transpileWhereClause(`a > 5 or b = 7 and c = 3`);
+    const expected: Condition = {
+        type: 'or',
+        variables: [
+            { fields: ['a'], operator: '>', value: '5' },
+            {
+                type: 'and',
+                variables: [
+                    { fields: ['b'], operator: '==', value: '7' },
+                    { fields: ['c'], operator: '==', value: '3' },
+                ],
+            },
+        ],
+    };
+    expect(whereClause.filters).toEqual(expected);
+});
+
+test('complex and / or', () => {
+    const whereClause = transpileWhereClause(`((a > 5 or (b = 7)) and ((c != 5))) and d = 99 or (e <> 25 or f = 9)`);
+    const expected: Condition = {
+        type: 'or',
+        variables: [
+            {
+                type: 'and',
+                variables: [
+                    {
+                        type: 'and',
+                        variables: [
+                            {
+                                type: 'or',
+                                variables: [
+                                    { fields: ['a'], operator: '>', value: '5' },
+                                    { fields: ['b'], operator: '==', value: '7' },
+                                ],
+                            },
+                            { fields: ['c'], operator: '!=', value: '5' },
+                        ],
+                    },
+                    { fields: ['d'], operator: '==', value: '99' },
+                ],
+            },
+            {
+                type: 'or',
+                variables: [
+                    { fields: ['e'], operator: '!=', value: '25' },
+                    { fields: ['f'], operator: '==', value: '9' },
+                ],
+            },
+        ],
+    };
+    expect(whereClause.filters).toEqual(expected);
+});
+
+test('arithmetic (addition) 1', () => {
+    const whereClause = transpileWhereClause(`"water_level" + 2 > 11.9`);
+    const expected: Filter = { fields: ['water_level'], fieldsPattern: '$ + 2', operator: '>', value: '11.9' };
+    expect(whereClause.filters).toEqual(expected);
+});
+
+test('arithmetic (addition) 2', () => {
+    const whereClause = transpileWhereClause(`"A a" + "B" = 10`);
+    const expected: Filter = { fields: ['"A a"', 'B'], fieldsPattern: '$ + $', operator: '==', value: '10' };
+    expect(whereClause.filters).toEqual(expected);
+});
+
+test('arithmetic (subtraction) 1', () => {
+    const whereClause = transpileWhereClause(`1 -   a - "b b bb"   -  'c' <= 3`);
+    const expected: Filter = { fields: ['a', '"b b bb"', 'c'], fieldsPattern: '1 - $ - $ - $', operator: '<=', value: '3' };
+    expect(whereClause.filters).toEqual(expected);
+});
+
+test('arithmetic (subtraction) 2', () => {
+    const whereClause = transpileWhereClause(`-a < -2.5`);
+    const expected: Filter = { fields: ['a'], fieldsPattern: '-$', operator: '<', value: '-2.5' };
+    expect(whereClause.filters).toEqual(expected);
+});
+
+test('arithmetic (multiplication) 1', () => {
+    const whereClause = transpileWhereClause(`a * 10 >= 20`);
+    const expected: Filter = { fields: ['a'], fieldsPattern: '$ * 10', operator: '>=', value: '20' };
+    expect(whereClause.filters).toEqual(expected);
+});
+
+test('arithmetic (multiplication) 2', () => {
+    const whereClause = transpileWhereClause(`(a + "b") * 3.5 - -1 > 999`);
+    const expected: Filter = { fields: ['a', 'b'], fieldsPattern: '($ + $) * 3.5 - -1', operator: '>', value: '999' };
+    expect(whereClause.filters).toEqual(expected);
+});
+
+test('arithmetic (division)', () => {
+    const whereClause = transpileWhereClause(`100 / 'A' / "B" != 2`);
+    const expected: Filter = { fields: ['A', 'B'], fieldsPattern: '100 / $ / $', operator: '!=', value: '2' };
+    expect(whereClause.filters).toEqual(expected);
+});
+
+test('arithmetic (modulo)', () => {
+    const whereClause = transpileWhereClause(`a % 2 = 0`);
+    const expected: Filter = { fields: ['a'], fieldsPattern: '$ % 2', operator: '==', value: '0' };
+    expect(whereClause.filters).toEqual(expected);
+});
+
+test('arithmetic (bitwise and)', () => {
+    const whereClause = transpileWhereClause(`(a & 15 > 0)`);
+    const expected: Filter = { fields: ['a'], fieldsPattern: '$ & 15', operator: '>', value: '0' };
+    expect(whereClause.filters).toEqual(expected);
+});
+
+test('arithmetic (bitwise or)', () => {
+    const whereClause = transpileWhereClause(`((a | b) <> 0)`);
+    const expected: Filter = { fields: ['a', 'b'], fieldsPattern: '$ | $', operator: '!=', value: '0' };
+    expect(whereClause.filters).toEqual(expected);
+});
+
+test('arithmetic (bitwise exclusive-or)', () => {
+    const whereClause = transpileWhereClause(`a ^ true = 2`);
+    const expected: Filter = { fields: ['a'], fieldsPattern: '$ ^ true', operator: '==', value: '2' };
+    expect(whereClause.filters).toEqual(expected);
+});
---
+++ src/logic/transpileWhereClause.ts
@@ -0,0 +1,154 @@
+export interface Filter {
+    fields: string[];
+    fieldsPattern?: string;
+    operator: string;
+    value: string;
+}
+
+export interface Condition {
+    type: 'and' | 'or',
+    variables: (Condition | Filter)[];
+}
+
+interface WhereClause {
+    filters?: Condition | Filter;
+}
+
+const REGEX_NUMBER = /^(-?[0-9]+(?:\.?[0-9]+)?)$/;
+const REGEX_TIMESTAMP = /^([0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z)$/;
+const REGEX_COMPARISON_OPERATORS = /<=|>=|<>|!=|=~|!~|<|>|==|=/;
+
+export const transpileWhereClause = (influxQL: string): WhereClause => {
+    if (influxQL.trim().length === 0)
+        return {};
+
+    return { filters: parse(influxQL) };
+};
+
+const parse = (influxQL: string): Condition | Filter => {
+    influxQL = removeUnnecessaryOuterBrackets(influxQL);
+
+    const orGroups = getMostOuterGroups(influxQL, 'or');
+    if (orGroups.length > 1)
+        return { type: 'or', variables: orGroups.map(parse) } as Condition;
+
+    const andGroups = getMostOuterGroups(influxQL, 'and');
+    if (andGroups.length > 1)
+        return { type: 'and', variables: andGroups.map(parse) } as Condition;
+
+    return parseFilter(influxQL);
+};
+
+const parseFilter = (influxQL: string): Filter => {
+    const { fields, fieldsPattern } = formatFields(influxQL);
+    const operator = formatOperator(influxQL);
+    const value = formatValue(influxQL, operator);
+
+    if (fieldsPattern === '$')
+        return { fields, operator, value } as Filter;
+
+    return { fields, fieldsPattern, operator, value } as Filter;
+};
+
+const formatFields = (influxQL: string): { fields: string[], fieldsPattern: string } => {
+    const left = influxQL.split(REGEX_COMPARISON_OPERATORS)[0];
+
+    let fieldsPattern: string = left.trim();
+
+    const fields: string[] = [];
+    for (const fieldMatch of left.trim().matchAll(/([a-zA-Z_]+)|"([^"]+)"|'([^']+)'/gi)) {
+        const field: string = fieldMatch[1] ?? fieldMatch[2] ?? fieldMatch[3] ?? fieldMatch[0];
+
+        if (field.toLowerCase() === 'true' || field.toLowerCase() === 'false')
+            continue;
+
+        fields.push(field.includes(' ') ? `"${field}"` : field);
+        fieldsPattern = fieldsPattern.replace(fieldMatch[0], '$');
+    }
+
+    fieldsPattern = fieldsPattern.replace(/  +/g, ' ').trim();
+    fieldsPattern = removeUnnecessaryOuterBrackets(fieldsPattern);
+
+    return { fields, fieldsPattern };
+};
+
+const formatOperator = (influxQL: string): string => {
+    const operatorMatch = influxQL.match(REGEX_COMPARISON_OPERATORS);
+    if (!operatorMatch)
+        return '==';
+
+    let operator = operatorMatch[0];
+    if (operator === '=')
+        operator = '==';
+
+    if (operator === '<>')
+        operator = '!=';
+
+    return operator;
+};
+
+const formatValue = (influxQL: string, operator: string): string => {
+    let value = (influxQL.split(REGEX_COMPARISON_OPERATORS)[1] ?? '').trim();
+
+    const valueMatch =
+        value.match(/^'([^']*)'$/) ??
+        value.match(/^"([^"]*)"$/) ??
+        value.match(REGEX_NUMBER);
+
+    if (valueMatch)
+        value = valueMatch[1];
+
+    if (!value.match(REGEX_NUMBER) &&
+        !value.match(REGEX_TIMESTAMP) &&
+        !(operator === '=~' || operator === '!~') &&
+        !value.includes('now()'))
+        value = `"${value}"`;
+
+    return value;
+};
+
+const getMostOuterGroups = (influxQL: string, connective: 'and' | 'or'): string[] => {
+    const groups: string[] = [];
+
+    let currentGroupStartIndex = 0;
+    let bracketStack = 0;
+    for (let i = 0; i < influxQL.length; i++) {
+        if (influxQL.charAt(i) === '(')
+            bracketStack++;
+        else if (influxQL.charAt(i) === ')')
+            bracketStack--;
+
+        if (bracketStack === 0 && influxQL.substring(i).match(new RegExp(`^ ${connective} `, 'i'))) {
+            groups.push(influxQL.substring(currentGroupStartIndex, i));
+            currentGroupStartIndex = i + ` ${connective} `.length;
+        }
+    }
+
+    groups.push(influxQL.substring(currentGroupStartIndex));
+
+    return groups;
+};
+
+const removeUnnecessaryOuterBrackets = (influxQL: string): string => {
+    while (influxQL.startsWith('(') && influxQL.endsWith(')')) {
+        const newInfluxQL = influxQL.trim().substring(1, influxQL.length - 1).trim();
+
+        let bracketStack = 0;
+        for (let i = 0; i < newInfluxQL.length; i++) {
+            if (newInfluxQL.charAt(i) === '(')
+                bracketStack++;
+            else if (newInfluxQL.charAt(i) === ')')
+                bracketStack--;
+
+            if (bracketStack < 0)
+                break;
+        }
+
+        if (bracketStack < 0)
+            break;
+
+        influxQL = newInfluxQL;
+    }
+
+    return influxQL;
+};
