c6bee491e40d2ebc74edf9e821e527476cbe95f5
TwentyFive <TwentyFiveSoftware@gmail.com>
2022-03-28 20:46:48 +0200

    set current InfluxQL query as URL parameter

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,21 @@ import React, { useEffect, useState } from 'react';
 import styles from './styles/App.module.scss';
 import { transpile } from './logic/transpile';
 import TextareaAutosize from 'react-textarea-autosize';
+import { Base64 } from 'js-base64';
 import SyntaxHighlighting from './components/SyntaxHighlighting';
 
-const App = () => {
+const App: React.FC = () => {
     const [influxQL, setInfluxQL] = useState<string>('');
     const [flux, setFlux] = useState<string>('');
 
     useEffect(() => setFlux(transpile(influxQL)), [influxQL]);
+
+    useEffect(() => setInfluxQL(Base64.decode(new URLSearchParams(window.location.search).get('q') ?? '')), []);
+
+    const onInfluxQLChange = (value: string) => {
+        setInfluxQL(value);
+        window.history.pushState('', '', `/?q=${Base64.encode(value)}`);
+    };
 
     return (
         <div className={styles.app}>
@@ -17,7 +25,7 @@ const App = () => {
 
                 <div className={styles.inputContainer}>
                     <TextareaAutosize className={styles.input} rows={1} placeholder={'Type your InfluxQL query here'}
-                                      value={influxQL} onChange={e => setInfluxQL(e.target.value)} spellCheck={false} />
+                                      value={influxQL} onChange={e => onInfluxQLChange(e.target.value)} spellCheck={false} />
                 </div>
 
                 {flux.length > 0 && (
@@ -25,7 +33,6 @@ const App = () => {
                         <SyntaxHighlighting code={flux} />
                     </div>
                 )}
-
 
                 <a className={styles.github} href={'https://github.com/TwentyFiveSoftware/influxql2flux'}>
                     GitHub / TwentyFiveSoftware / influxql2flux
